27dc20ecdf34bd62d442c76010361eeb2c2a4a46
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-02-28 22:37:13 +0100

    Now you can click on a note and it will be displayed with the respective course associated to it.

--- app/src/main/java/dk/martin/notekeeper/MainActivity.kt
+++ app/src/main/java/dk/martin/notekeeper/MainActivity.kt
@@ -1,16 +1,15 @@
 package dk.martin.notekeeper
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
-
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var notePosition = POSITION_NOT_SET
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +22,21 @@ class MainActivity : AppCompatActivity() {
                 DataManager.courses.values.toList())
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerCourses.adapter = adapterCourses
+
+        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+
+        if (notePosition != POSITION_NOT_SET)
+            displayNote()
+
+    }
+
+    private fun displayNote() {
+        val note = DataManager.notes[notePosition]
+        textNoteTitle.setText(note.title)
+        textNoteText.setText(note.text)
+
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        spinnerCourses.setSelection(coursePosition)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/dk/martin/notekeeper/NoteListActivity.kt
+++ app/src/main/java/dk/martin/notekeeper/NoteListActivity.kt
@@ -2,10 +2,8 @@ package dk.martin.notekeeper
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
-
 import kotlinx.android.synthetic.main.activity_note_list.*
 import kotlinx.android.synthetic.main.content_note_list.*
 
@@ -21,7 +19,15 @@ class NoteListActivity : AppCompatActivity() {
             startActivity(activityIntent)
         }
 
-        listNotes.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataManager.notes)
+        listNotes.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            DataManager.notes
+        )
+        listNotes.setOnItemClickListener { _, _, position, _ ->
+            val activityIntent = Intent(this, MainActivity::class.java)
+            activityIntent.putExtra(EXTRA_NOTE_POSITION, position)
+            startActivity(activityIntent)
+        }
     }
-
 }
